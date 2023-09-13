e2ec4da3b2f9dc641c2bf0d04cd03fdee792194e
Jakub <jm54706@outlook.com>
2023-09-13 17:29:40 +0200

    added checkbox

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,18 @@
 const navDrawer = document.querySelector('.nav-drawer');
 const navMenu = document.querySelector('.nav__menu');
 const blur = document.querySelector('.blur');
+const checkbox = document.querySelector('.task-box__checkbox');
 
 const activeBurgerMenu = () => {
 	navDrawer.classList.toggle('nav-drawer--active');
 	blur.classList.toggle('blur--active');
 };
 
+const activeCheck = () => {
+	if (checkbox.checked == true) {
+		console.log('działa');
+	}
+}
+
 navMenu.addEventListener('click', activeBurgerMenu);
+checkbox.addEventListener('click', activeCheck)
