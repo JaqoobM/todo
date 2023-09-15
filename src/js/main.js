f268da746dc6159f825f6939521661ed2c293556
Jakub <jm54706@outlook.com>
2023-09-15 17:10:42 +0200

    added edit popup

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,12 @@ const checkLabel = document.querySelectorAll('.task-box__check-label');
 const addBtn = document.querySelector('.navbar-bottom__circle');
 const body = document.querySelector('body');
 const createTaskBox = document.querySelector('.create-task-box');
+const closeBtn = document.querySelector('.create-task-box__btn-close');
+const task = document.querySelectorAll('.task-box__task');
+const editTaskBox = document.querySelector('.edit-task-box');
+const closeBtnEdit = document.querySelector('.edit-task-box__btn-close');
 
-const activeBurgerMenu = () => {
-	navDrawer.classList.toggle('nav-drawer--active');
-	blur.classList.toggle('blur--active');
-
+const bodyStop = () => {
 	if (body.style.overflow == 'hidden') {
 		body.style.overflow = 'visible';
 	} else {
@@ -17,21 +18,49 @@ const activeBurgerMenu = () => {
 	}
 };
 
+const activeBurgerMenu = () => {
+	navDrawer.classList.toggle('nav-drawer--active');
+	blur.classList.toggle('blur--active');
+	bodyStop();
+};
+
 const activeCheck = (e) => {
 	const label = e.target;
 	const task = label.parentElement.parentElement;
-
 	label.classList.toggle('task-box__check-label--active');
 	task.classList.toggle('task-box__task--active');
 };
 
-const addPopup = () => {
-	console.log('działa');
+const showAddPopup = () => {
 	createTaskBox.classList.add('create-task-box--active');
+	blur.classList.add('blur--active');
+	blur.style.zIndex = 4;
+
+	bodyStop();
+};
+
+const showEditPopup = () => {
+	editTaskBox.classList.add('edit-task-box--active');
+	blur.classList.add('blur--active');
+	blur.style.zIndex = '';
+
+	bodyStop();
+};
+
+const closePopup = () => {
+	createTaskBox.classList.remove('create-task-box--active');
+	editTaskBox.classList.remove('edit-task-box--active');
+	const input = document.querySelectorAll('input');
+	input.forEach((x) => (x.value = ''));
+	blur.classList.remove('blur--active');
+	blur.style.zIndex = '';
+
+	bodyStop();
 };
 
 navMenu.addEventListener('click', activeBurgerMenu);
-addBtn.addEventListener('click', addPopup);
-checkLabel.forEach((x) => {
-	x.addEventListener('click', activeCheck);
-});
+closeBtn.addEventListener('click', closePopup);
+closeBtnEdit.addEventListener('click', closePopup);
+addBtn.addEventListener('click', showAddPopup);
+checkLabel.forEach((x) => x.addEventListener('click', activeCheck));
+task.forEach((x) => x.addEventListener('click', showEditPopup));
